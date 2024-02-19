359dc701bc8aa566bb93d761ed17d130d763ce11
HP <madhankumarchavan26@gmail.com>
2024-02-19 15:12:09 +0530

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,38 +1,3 @@
-//def call() {
-//
-//    pipeline {
-//        agent {
-//            label 'workstation'
-//        }
-//
-//        stages {
-//            stage('Build/compile') {
-//                steps {
-//                    echo 'compile'
-//                }
-//            }
-//            stage('unit Test') {
-//                steps {
-//                    echo 'unit tests'
-//                }
-//            }
-//            stage('quality control') {
-//                steps {
-//                    echo 'quality control'
-//                }
-//            }
-//            stage('upload in centralized place') {
-//                steps {
-//                    echo 'upload'
-//                }
-//            }
-//        }
-//    }
-//
-//}
-//
-
-
 def call() {
 
     pipeline {
@@ -43,15 +8,29 @@ def call() {
         stages {
             stage('Build/compile') {
                 steps {
-                    script{
-                        def abc ="hello"
-                        def xyz ="10"
-                        print "abc= $(abc)"
-                        print "xyz = $(xyz)"
-                    }
+                   script{
+                       commom.compile()
+                   }
+                }
+            }
+            stage('unit Test') {
+                steps {
+                    echo 'unit tests'
+                }
+            }
+            stage('quality control') {
+                steps {
+                    echo 'quality control'
+                }
+            }
+            stage('upload in centralized place') {
+                steps {
+                    echo 'upload'
                 }
             }
         }
-
     }
+
 }
+
+
